b06a1e2f0c6c72ccaa15693f99c6b8dbf35205cf
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-05-24 14:43:12 +0100

    test

---
+++ build/precache-manifest.de52ae9e50c203354961f7bacf943d6e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8887527fcb2ed95d6655",
+    "url": "/static/js/main.e725d258.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c134acc80dec8d69f2f6",
+    "url": "/static/js/2.f929a62e.chunk.js"
+  },
+  {
+    "revision": "83adf504735e16918842d6078ab07dbc",
+    "url": "/index.html"
+  }
+];
